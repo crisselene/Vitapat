d2a68ee32bc0c755ddb20d596b1f9dc829d6ed5b
21732599 <criisalways@gmail.com>
2019-02-21 11:54:41 +0100

    gps no finished and item main

--- app/src/main/java/com/example/a21732599/pruebas/MainActivity.java
+++ app/src/main/java/com/example/a21732599/pruebas/MainActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGPS = findViewById(R.id.btnMap);
+
+    }
+
+    public void abrirMapa(View v){
+        Intent i = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(i);
     }
 
     public void goProfile(View v) {
