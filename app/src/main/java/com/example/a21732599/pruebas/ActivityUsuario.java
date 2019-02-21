2da1fed07b8924e7c510888eb7e0b638edd419a8
alavrosone <alvaropokesone@gmail.com>
2019-02-21 11:36:26 +0100

    user

--- app/src/main/java/com/example/a21732599/pruebas/ActivityUsuario.java
+++ app/src/main/java/com/example/a21732599/pruebas/ActivityUsuario.java
@@ -2,6 +2,7 @@ package com.example.a21732599.pruebas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityUsuario extends AppCompatActivity {
 
@@ -12,4 +13,16 @@ public class ActivityUsuario extends AppCompatActivity {
     }
 
     //TODO: COGER EL NOMBRE DE USUARIO
+
+    public void comentarios(View v) {
+
+    }
+
+    public void fotos(View v) {
+
+    }
+
+    public void favoritos(View v) {
+
+    }
 }
