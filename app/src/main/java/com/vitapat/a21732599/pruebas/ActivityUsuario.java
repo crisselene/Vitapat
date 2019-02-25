e743f6eb04a0dfba741a8f0fb3845e7dd6c7dd42
21745694 <alvaropoesone@gmail.com>
2019-02-25 18:50:14 +0100

    Menu

--- app/src/main/java/com/vitapat/a21732599/pruebas/ActivityUsuario.java
+++ app/src/main/java/com/vitapat/a21732599/pruebas/ActivityUsuario.java
@@ -2,6 +2,9 @@ package com.vitapat.a21732599.pruebas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class ActivityUsuario extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class ActivityUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_user, popup.getMenu());
+        popup.show();
     }
 
     //TODO: COGER EL NOMBRE DE USUARIO
