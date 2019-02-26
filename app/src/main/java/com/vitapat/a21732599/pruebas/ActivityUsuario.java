87a5863dbfaf04aba47ecc8353bba2fd836e9b6d
21745694 <alvaropokesone@gmail.com>
2019-02-26 17:07:33 +0100

    Cerrar sesión

--- app/src/main/java/com/vitapat/a21732599/pruebas/ActivityUsuario.java
+++ app/src/main/java/com/vitapat/a21732599/pruebas/ActivityUsuario.java
@@ -1,25 +1,64 @@
 package com.vitapat.a21732599.pruebas;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.PopupMenu;
 
 public class ActivityUsuario extends AppCompatActivity {
+
+    Button btnPower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
+
+        btnPower = findViewById(R.id.btnPower);
+
+       btnPower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityUsuario.this,
+                        R.style.MyDialogTheme);
+
+                builder.setMessage("¿Estás seguro de que quieres cerrar sesión?");
+
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent(ActivityUsuario.this, LoginActivity.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(i);
+                    }
+
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+
+                alert.show();
+            }
+        });
     }
 
-    public void showPopup(View v) {
-        PopupMenu popup = new PopupMenu(this, v);
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.menu_user, popup.getMenu());
-        popup.show();
-    }
+
+
 
     //TODO: COGER EL NOMBRE DE USUARIO
 }
